fa8a1c66f3804ebdfb41aaddc57ef788ae5120cf
Nandhu1627 <nandhusivasubramaniam97@gmail.com>
2017-07-14 10:30:15 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class code
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        //String s1=s.nextLine();
+        char a = s.next(".").charAt(0);
+
+        //for(int i=0;i<s1.length();i++)
+        //{
+            if(Character.isLetter(a))
+            System.out.print("Is a alphabet");
+            else
+            System.out.print("Not a alphabet");
+            }
+}
