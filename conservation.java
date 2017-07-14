fc903085484482c0c053b077691c45d5d9beaaea
Nandhu1627 <nandhusivasubramaniam97@gmail.com>
2017-07-14 15:24:38 +0530

    Create conservation.java

---
+++ conservation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class conversion
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        System.out.print(Integer.parseInt(s1));
+    }
+}
